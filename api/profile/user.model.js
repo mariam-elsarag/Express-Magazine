8b02bc62abda1a1a0d8eee397a34313af2209fd3
mariam-elsarag <mariamelsarag44@gmail.com>
2025-01-31 19:53:13 +0200

    adjust user model

--- api/profile/user.model.js
+++ api/profile/user.model.js
@@ -2,83 +2,96 @@ import mongoose from "mongoose";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
-const userSchema = new mongoose.Schema({
-  full_name: {
-    type: String,
-    required: [true, "Full name is required"],
-    minlength: [3, "Full name should be at least 3 characters long"],
-    maxlength: [50, "Full name should not exceed 50 characters"],
-  },
-  email: {
-    type: String,
-    required: [true, "Email is required"],
-    unique: true,
-    match: [
-      /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-      "Please enter a valid email address",
-    ],
-  },
-  googleId: { type: String, unique: true },
-  role: {
-    type: String,
-    enum: ["user", "admin"],
-    default: "user",
-  },
-  avatar: {
-    type: String,
-  },
-  cover_image: {
-    type: String,
-  },
-  rating_quantity: {
-    type: Number,
-    default: 0,
-  },
-  rating_average: {
-    type: Number,
-    default: null,
-    enum: [1, 2, 3, 4, 5],
-    set: (val) => Math.round(val * 10) / 10,
-  },
-  followers: {
-    type: Number,
-    default: 0,
-  },
-  password: {
-    type: String,
-    required: [
-      function () {
-        return !this.googleId;
-      },
-      "Password is required",
-    ],
-    match: [
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
-      "Week password",
-    ],
-    select: false,
-  },
+const userSchema = new mongoose.Schema(
+  {
+    full_name: {
+      type: String,
+      required: [true, "Full name is required"],
+      minlength: [3, "Full name should be at least 3 characters long"],
+      maxlength: [50, "Full name should not exceed 50 characters"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      match: [
+        /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+        "Please enter a valid email address",
+      ],
+    },
+    googleId: { type: String, unique: true },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+    avatar: {
+      type: String,
+    },
+    cover_image: {
+      type: String,
+    },
+    rating_quantity: {
+      type: Number,
+      default: 0,
+    },
+    rating_average: {
+      type: Number,
+      default: null,
+      enum: [1, 2, 3, 4, 5],
+      set: (val) => Math.round(val * 10) / 10,
+    },
+    followers: {
+      type: Number,
+      default: 0,
+    },
+    password: {
+      type: String,
+      required: [
+        function () {
+          return !this.googleId;
+        },
+        "Password is required",
+      ],
+      match: [
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+        "Week password",
+      ],
+      select: false,
+    },
 
-  password_changed_at: {
-    type: Date,
-    default: null,
-    select: false,
+    password_changed_at: {
+      type: Date,
+      default: null,
+      select: false,
+    },
+    otp_verified_token: {
+      type: String,
+      select: false,
+    },
+    otp: {
+      type: String,
+      default: null,
+      select: false,
+    },
+    otp_expired_at: {
+      type: Date,
+      default: null,
+      select: false,
+    },
   },
-  otp_verified_token: {
-    type: String,
-    select: false,
-  },
-  otp: {
-    type: String,
-    default: null,
-    select: false,
-  },
-  otp_expired_at: {
-    type: Date,
-    default: null,
-    select: false,
-  },
-});
+  {
+    toJSON: {
+      transform: (doc, ret) => {
+        ret.user_id = ret._id;
+
+        delete ret._id;
+        delete ret.id;
+        delete ret.__v;
+      },
+    },
+  }
+);
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
   this.password = await bcrypt.hash(this.password, 12);
